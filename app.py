edd17b66ebdc0019bae4e5b26fec64a81b33a469
TacoTruckMan777 <jcruz91011@gmail.com>
2025-10-29 08:43:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-import json
-## Open the JSON file of movie data
-movies = open("./movies.json", encoding="utf8")
-## create variable "data" that represents the enitre movie list
-data = json.load(movies)
-
+# import json
+# ## Open the JSON file of movie data
+# movies = open("./movies.json", encoding="utf8")
+# ## create variable "data" that represents the enitre movie list
+# data = json.load(movies)
+# print(f"movie names ")
+def perfection():
+    for i in range(99999):
+        print("67")
+perfection()
