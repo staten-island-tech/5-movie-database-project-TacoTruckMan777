baaee03de52eb31ba7134caf26b7292d8cab9369
TacoTruckMan777 <jcruz91011@gmail.com>
2025-10-30 09:25:38 -0400

    save

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+# import json
+# q = 0
+# ## Open the JSON file of movie data
+# movies = open("./movies.json", encoding="utf8")
+# ## create variable "data" that represents the enitre movie list
+# data = json.load(movies)
+# for i in range(14117):
+#     print(data[q]['title'])
+#     q += 1
+
 import json
 q = 0
-## Open the JSON file of movie data
 movies = open("./movies.json", encoding="utf8")
-## create variable "data" that represents the enitre movie list
 data = json.load(movies)
-for i in range(10000):
+for i in range(14117):
     print(data[q]['title'])
     q += 1
